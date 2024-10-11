0209fb29fc988b5ad3528a52568f3359bf7ade8a
RiskiManik <riskifirman608@gmail.com>
2024-10-11 20:42:02 +0700

    Pegumpulan Tugas pertemuan 1

---
+++ latihan1/index.php
@@ -0,0 +1,31 @@
+<?php
+$angka1 = 10;
+$angka2 = 20;
+
+$tambah = $angka1 + $angka2;
+$kurang = $angka1 - $angka2;
+$kali = $angka1 * $angka2;
+$bagi = $angka1 / $angka2;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <p>angka1 : <?php echo $angka1  ?> </p>
+    <p>angka2 : <?php echo $angka2  ?> </p>
+    <p>tambah : <?php echo $tambah  ?> </p>
+    <p>Kurang : <?php echo $kurang  ?> </p>
+    <p>Bagi : <?php echo $bagi  ?> </p>
+    <p>Kali : <?php echo $kali  ?> </p>
+</body>
+
+</html>
